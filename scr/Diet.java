0107c0033d0cacab96b13422778a032e69463fac
Cooljean <Cooljean95@ymail.com>
2021-04-09 21:22:45 +0200

    la til ting på Diet

--- scr/Diet.java
+++ scr/Diet.java
@@ -1,2 +1,22 @@
 public class Diet {
+
+
+    int daysDuration;
+    String purpose;
+    Food allowedFood[];
+    boolean isVegan;
+
+
+    public String writeDuraton(){
+        String Result ="";
+        return Result;
+    }
+
+    public String writeAlowedFood(){
+        String Result = "";
+        return Result;
+    }
+
+
+
 }
