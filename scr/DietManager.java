53206121a3899531f25729118f81b3d168b3005d
Sondre Sovik <sondre.sovik@nostdahl.com>
2021-04-09 20:52:09 +0200

    legg inn litt kode

--- scr/DietManager.java
+++ scr/DietManager.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
 public class DietManager {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+    }
+
+    public void randomDiet() {
+        Person p = new Person();
+        ArrayList<Food> food = new ArrayList<>();
     }
 }
--- scr/Food.java
+++ scr/Food.java
@@ -1,2 +1,5 @@
 public class Food {
+    String name;
+    int caloriePer100g;
+
 }
