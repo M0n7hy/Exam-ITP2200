ec5cc46306b1578dcaab19fd53765d0f5f594dee
Sondre Sovik <sondre.sovik@nostdahl.com>
2021-04-11 22:30:00 +0200

    La til en override metode i FlexitarianDiet

--- scr/Exam2021/FlexitarianDiet.java
+++ scr/Exam2021/FlexitarianDiet.java
@@ -10,6 +10,13 @@ public class FlexitarianDiet extends Diet {
 
     @Override
     void Diet() {
-
+        daysDuration = 150;
+        purpose = "";
+        allowedFood = new Food[]{new Food("Rice", 130, true, FoodType.Protein), new Food("Salad", 200, true, FoodType.Recipe),
+                new Food("Salmon", 100, false, FoodType.Protein), new Food("Bread", 80, true, FoodType.Carb),
+                new Food("Cod", 82, false, FoodType.Protein), new Food("Potato", 76, true, FoodType.Carb),
+                new Food("")
+        };
+        isVegan = false;
     }
 }
