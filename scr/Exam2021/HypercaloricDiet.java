0d9d0fc6f86b9585743d6805d94acfa31e9c69e7
Sondre Sovik <sondre.sovik@nostdahl.com>
2021-04-13 22:01:49 +0200

    Implementerte writeAllowedFood og testen til den. (Fungerer)

--- scr/Exam2021/HypercaloricDiet.java
+++ scr/Exam2021/HypercaloricDiet.java
@@ -5,14 +5,22 @@ public class HypercaloricDiet extends Diet {
     float maxWeighKg;
     float minCaloriesPerDay;
 
+    public HypercaloricDiet() {
+        Diet();
+    }
+
 
     @Override
     void Diet() {
-
         daysDuration = 200;
-        purpose = "weight lost";
-        allowedFood = new Food[]{new Food("Chicken", 150, false, FoodType.Protein), new Food("Salad", 200, true, FoodType.Recipe),
-                new Food("Rice", 300, true, FoodType.Carb), new Food("Salmon", 100, false, FoodType.Protein), new Food("Bread", 80, true, FoodType.Carb)
+        purpose = "max calories";
+        allowedFood = new Food[]{
+                new Food("Chicken", 150, false, FoodType.Protein),
+                new Food("Salad", 200, true, FoodType.Recipe),
+                new Food("Rice", 300, true, FoodType.Carb),
+                new Food("Salmon", 100, false, FoodType.Protein),
+                new Food("Bread", 80, true, FoodType.Carb),
+                new Food("Steak", 270, false, FoodType.Protein)
         };
         isVegan = false;
 
