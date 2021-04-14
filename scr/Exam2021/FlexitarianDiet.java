5d0c5acf9892fe7205623d0b356701f3ea7565e7
Sondre Sovik <sondre.sovik@nostdahl.com>
2021-04-14 20:51:10 +0200

    Prøve på req 4b

--- scr/Exam2021/FlexitarianDiet.java
+++ scr/Exam2021/FlexitarianDiet.java
@@ -15,9 +15,12 @@ public class FlexitarianDiet extends Diet {
         daysDuration = 150;
         purpose = "Keep in shape";
         allowedFood = new Food[]{
+                //1d.The preferred meat in a FlexitarianDiet MUST be non-vegan food of protein type.
                 new Food("Rice", 130, true, FoodType.Protein),
                 new Food("Salad", 200, true, FoodType.Recipe),
+                //1d.The preferred meat in a FlexitarianDiet MUST be non-vegan food of protein type.
                 new Food("Salmon", 100, false, FoodType.Protein),
+                //1d.The preferred meat in a FlexitarianDiet MUST be non-vegan food of protein type.
                 new Food("Cod", 82, false, FoodType.Protein),
                 new Food("Bread", 80, true, FoodType.Carb),
                 new Food("Potato", 76, true, FoodType.Carb),
@@ -27,7 +30,7 @@ public class FlexitarianDiet extends Diet {
                 new Food("Strawberry", 33, true, FoodType.Carb)
 
         };
-        isVegan = false;
+        //isVegan = false;
     }
 
     public FlexitarianDiet(String name,Food[] allowedFood){
