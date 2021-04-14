e0721e29d38ca907426a27532d693362bb5bff7a
Sondre Sovik <sondre.sovik@nostdahl.com>
2021-04-14 13:40:51 +0200

    små tweeks på forje push.

--- test/Exam2021/DietTest.java
+++ test/Exam2021/DietTest.java
@@ -13,17 +13,38 @@ public class DietTest {
     public void LowcarbTest(){
         LowcarbDiet lowCarb = new LowcarbDiet();
         lowCarb.LowcarbDiet(55);
+        //lowCarb.LowcarbDiet(47); /*Test for exception*/
+
+        VeganDiet vegan = new VeganDiet();
+        vegan.minweightkg(65);
+        //vegan.minweightkg(35); /*Test for exception*/
 
     }
 
 
     @Test
-    public void DietTest(){
+    public void durationTest(){
         LowcarbDiet low = new LowcarbDiet();
         low.dietDuration("Vegan Diet", new Date(2019,2,24),new Date(2020, 8, 29));
 
-        String res = low.writeDuraton();
-        assertEquals("Vegan Diet lasts for 1 years, 6 months and 5 days.", res);
+        HypercaloricDiet hyper = new HypercaloricDiet();
+        hyper.dietDuration("Hypercaloric Diet", new Date(2020, 4, 10), new Date(2020, 10, 27));
+
+        VeganDiet vegan = new VeganDiet();
+        vegan.dietDuration("Vegen Diet", new Date(2012, 12, 10), new Date(2013, 6, 28));
+
+        FlexitarianDiet flex = new FlexitarianDiet();
+        flex.dietDuration("Flexitarian Diet", new Date(2021, 4, 14), new Date(2021, 9, 11));
+
+        String resLow = low.writeDuraton();
+        String resHyper = hyper.writeDuraton();
+        String resVegan = vegan.writeDuraton();
+        String resFlex = flex.writeDuraton();
+
+        assertEquals("Vegan Diet lasts for 1 years, 6 months and 5 days.", resLow);
+        assertEquals("Hypercaloric Diet lasts for 0 years, 6 months and 17 days.", resHyper);
+        //assertEquals("Vegan Diet lasts for 0 years, 6 months and 18 days.",resVegan);
+        //assertEquals("Flexitarian Diet lasts for 0 years, 4 months and 28 days", resFlex);
     }
 
     @Test
