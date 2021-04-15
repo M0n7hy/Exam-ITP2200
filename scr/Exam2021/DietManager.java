d3544ba194e963c48e929395be274e6c6b40d0cc
Sondre Sovik <sondre.sovik@nostdahl.com>
2021-04-15 19:48:42 +0200

    prøver å gjøre 4b

--- scr/Exam2021/DietManager.java
+++ scr/Exam2021/DietManager.java
@@ -30,15 +30,15 @@ public class DietManager {
         Random random = new Random();
         int min1 = 1;
         int max1 = 100;
-        int res1 = random.nextInt(min1 - max1) + min1;
+        int res1 = random.nextInt(max1 - min1) + min1;
 
         int min2 = 2000;
         int max2 = 4000;
-        int res2 = random.nextInt(min2 - max2) + min2;
+        int res2 = random.nextInt(max2 - min2) + min2;
 
-        float min3 = person.weight;
-        float max3 = person.weight + 20;
-        int res3 = (int) (random.nextInt((int) (min3 - max3)) + min3);
+        float min3 = person.getWeight();
+        float max3 = person.getWeight() + 20;
+        float res3 = (int) (random.nextInt((int) (max3 - min3)) + min3);
 
 
         HypercaloricDiet randomDiet = new HypercaloricDiet();
@@ -48,13 +48,40 @@ public class DietManager {
         randomDiet.isVegan = false;
         randomDiet.maxWeighKg = res3;
         randomDiet.minCaloriesPerDay = res2;
+
+        System.out.println(res1);
+        System.out.println(res2);
+        System.out.println(res3);
     }
 
 
 
     public HypercaloricDiet randomDiet(Person person, Food[] food){
+        Random random = new Random();
+        int min1 = 1;
+        int max1 = 100;
+        int res1 = random.nextInt(max1 - min1) + min1;
 
+        int min2 = 2000;
+        int max2 = 4000;
+        int res2 = random.nextInt(max2 - min2) + min2;
+
+        float min3 = person.getWeight();
+        float max3 = person.getWeight() + 20;
+        float res3 = (int) (random.nextInt((int) (max3 - min3)) + min3);
+
+
+        HypercaloricDiet randomDiet = new HypercaloricDiet();
+        randomDiet.daysDuration = res1;
+        randomDiet.purpose = "Random Diet";
+        randomDiet.allowedFood = food;
+        randomDiet.isVegan = false;
+        randomDiet.maxWeighKg = res3;
+        randomDiet.minCaloriesPerDay = res2;
+
+        System.out.println(res1);
+        System.out.println(res2);
+        System.out.println(res3);
         return null;
     }
-
 }
