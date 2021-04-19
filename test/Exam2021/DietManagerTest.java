66fa56965dcaaa6fb994048d09c151105a881ebe
Cooljean <Cooljean95@ymail.com>
2021-04-19 21:34:12 +0200

    Lagt til tester i de nye Testfilene.

--- test/Exam2021/DietManagerTest.java
+++ test/Exam2021/DietManagerTest.java
@@ -9,6 +9,36 @@ import static org.junit.Assert.assertTrue;
 public class DietManagerTest {
 
 
+    /************************ Requirements 4 ***************************/
+
+    @Test
+    public void randomPersonRandomDietTest(){
+        Person person = new Person();
+        HypercaloricDiet h1 = new HypercaloricDiet(80);
+        DietManager dietManager = new DietManager();
+
+
+        boolean res = dietManager.areCompatible(person, h1);
+        if (!res) {
+            throw new IllegalArgumentException("This diet is not compatible to this person.");
+        }
+        assertEquals(true, res);
+        System.out.println("Requirement 4.a: " + res + '\n');
+
+    }
+
+    @Test
+    public void personAndListOfFoodTest(){
+        Person person = new Person();
+        Food[] food = new Food[]{
+                new Food()
+        };
+        System.out.println("Requirement 4.a: ");
+        DietManager newdiet = new DietManager();
+
+        assertTrue(String.valueOf(newdiet.randomDiet(person, food)), true);
+
+    }
 
 
 }
